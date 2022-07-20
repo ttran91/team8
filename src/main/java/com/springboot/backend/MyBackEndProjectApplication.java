bbe08de4a5ea79e744220af210553617c1d6a3cf
Chan Kim <60370096+chkz6d@users.noreply.github.com>
2022-07-20 16:01:28 -0500

    Add files via upload

---
+++ src/main/java/com/springboot/backend/MyBackEndProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBackEndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBackEndProjectApplication.class, args);
+	}
+
+}
