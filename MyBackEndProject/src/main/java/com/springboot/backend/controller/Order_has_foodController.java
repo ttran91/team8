9d411735f79ff16f70b54a8ae97996955aac6ae9
Umair Ali <umairmamoor@gmail.com>
2022-07-29 08:53:23 -0400

    OrderHasFood POST

--- MyBackEndProject/src/main/java/com/springboot/backend/controller/Order_has_foodController.java
+++ MyBackEndProject/src/main/java/com/springboot/backend/controller/Order_has_foodController.java
@@ -1,8 +1,85 @@
 package com.springboot.backend.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.springboot.backend.model.Food;
+import com.springboot.backend.model.Order;
+import com.springboot.backend.model.Order_has_food;
+import com.springboot.backend.repository.FoodRepository;
+import com.springboot.backend.repository.OrderRepository;
+import com.springboot.backend.repository.Order_has_foodRepository;
 
 @RestController
 public class Order_has_foodController {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private FoodRepository foodRepository;
+	
+	@Autowired
+	private Order_has_foodRepository OrderHasFoodRepository;
+	
+	
+	
+	@PostMapping("/OrderHasFood/{oid}/{fid}")
+	public Order_has_food postOrder_has_food(@RequestBody Order_has_food OrderHasFood, 
+											@PathVariable("oid") Long oid,
+											@PathVariable("fid") Long fid) {
+
+		Optional<Order> optionalO = orderRepository.findById(oid);
+		if (!optionalO.isPresent())
+			throw new RuntimeException("Order ID is Invalid!!");
+
+		Optional<Food> optionalF = foodRepository.findById(fid);
+		
+		if(!optionalF.isPresent())
+			throw new RuntimeException("Food ID is Invalid!!");
+		
+		Order order = optionalO.get();
+		Food food = optionalF.get();
+		
+		OrderHasFood.setOrder(order);
+		OrderHasFood.setFood(food);
+	
+		return OrderHasFoodRepository.save(OrderHasFood);
+	}
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
--- MyBackEndProject/src/main/java/com/springboot/backend/model/Order_has_food.java
+++ MyBackEndProject/src/main/java/com/springboot/backend/model/Order_has_food.java
@@ -1,49 +1,49 @@
 package com.springboot.backend.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity  //means a class can be mapped to a table
+@Table(name="OrderHasFood")
 public class Order_has_food {
 	@Id //PK
 	@GeneratedValue(strategy = GenerationType.AUTO) //Auto increment
 	private Long id;
 	
-	@Column (nullable = true)
-	private String food_id;
-
-	
 	@OneToOne
 	private Order order; //FK
 
 	
-	/*
+	
 	@OneToOne
 	private Food food; //FK
-	*/
-	
-	
+
+
+
 	public Order_has_food() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
 
-	public Order_has_food(Long id, String orderId, String food_id, Order order) {
+
+	public Order_has_food(Long id, Order order, Food food) {
 		super();
 		this.id = id;
-		this.food_id = food_id;
 		this.order = order;
+		this.food = food;
 	}
+
 
 
 	public Long getId() {
 		return id;
 	}
+
 
 
 	public void setId(Long id) {
@@ -52,19 +52,10 @@ public class Order_has_food {
 
 
 
-	public String getFood_id() {
-		return food_id;
-	}
-
-
-	public void setFood_id(String food_id) {
-		this.food_id = food_id;
-	}
-
-
 	public Order getOrder() {
 		return order;
 	}
+
 
 
 	public void setOrder(Order order) {
@@ -72,18 +63,27 @@ public class Order_has_food {
 	}
 
 
+
+	public Food getFood() {
+		return food;
+	}
+
+
+
+	public void setFood(Food food) {
+		this.food = food;
+	}
+
+
+
 	@Override
 	public String toString() {
-		return "OrderHasFood [id=" + id + ", food_id=" + food_id + ", order=" + order + "]";
+		return "Order_has_food [id=" + id + ", order=" + order + ", food=" + food + "]";
 	}
-	
-	
-	
-	
 
 	
-
-
+	
+	
 
 
 
