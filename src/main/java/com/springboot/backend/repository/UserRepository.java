15e245b00f61e11a7ce5f6947e23941e35052eb7
Umair Ali <umairmamoor@gmail.com>
2022-08-03 16:36:27 -0400

    userRepository

--- src/main/java/com/springboot/backend/repository/UserRepository.java
+++ src/main/java/com/springboot/backend/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.springboot.backend.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.springboot.backend.model.UserInfo;
+
+public interface UserRepository extends JpaRepository<UserInfo, Long>{
+	
+	@Query("select u from UserInfo u where u.username=?1")
+	UserInfo getByUsername(String username);
 
 }
