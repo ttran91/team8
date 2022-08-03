998d6c722e0287236efab917a4732c13d7412fdb
Umair Ali <umairmamoor@gmail.com>
2022-08-03 17:03:55 -0400

    fixing the project

--- MyBackEndProject/src/main/java/com/springboot/backend/model/Order.java
+++ MyBackEndProject/src/main/java/com/springboot/backend/model/Order.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -22,17 +23,25 @@ public class Order {
 	
 	@Column (nullable = true)
 	private Double orderCost;
+	
+	@OneToOne
+	private Customer customer; //FK
+	
+	@OneToOne
+	private Vendor vendor;
 
 	public Order() {
 		super();
 		
 	}
 
-	public Order(Long id, String orderStatus, Double orderCost) {
+	public Order(Long id, String orderStatus, Double orderCost, Customer customer, Vendor vendor) {
 		super();
 		this.id = id;
 		this.orderStatus = orderStatus;
 		this.orderCost = orderCost;
+		this.customer = customer;
+		this.vendor = vendor;
 	}
 
 	public Long getId() {
@@ -59,10 +68,33 @@ public class Order {
 		this.orderCost = orderCost;
 	}
 
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Vendor getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(Vendor vendor) {
+		this.vendor = vendor;
+	}
+
 	@Override
 	public String toString() {
-		return "Order [id=" + id + ", orderStatus=" + orderStatus + ", orderCost=" + orderCost + "]";
+		return "Order [id=" + id + ", orderStatus=" + orderStatus + ", orderCost=" + orderCost + ", customer="
+				+ customer + ", vendor=" + vendor + "]";
 	}
+	
+	
+
+	
+	
+	
 	
 	
 	
