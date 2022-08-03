32af91680149faf0774ae08f4ebc57cbcde01fbe
Umair Ali <umairmamoor@gmail.com>
2022-08-03 16:48:17 -0400

    fixing structure of the project

--- MyBackEndProject/src/main/java/com/springboot/backend/repository/Order_has_foodRepository.java
+++ MyBackEndProject/src/main/java/com/springboot/backend/repository/Order_has_foodRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springboot.backend.model.Order_has_food;
 
 public interface Order_has_foodRepository extends JpaRepository<Order_has_food, Long>{
+
+    @Query("select r from Order_has_food r where r.food.id=?1")
+
+    List<Order_has_food> getOrderHasFoodByFid(Long fid);
+
+
+    @Query("select r from Order_has_food r where r.order.id=?1")
+    List<Order_has_food> getOrderHasFoodByOid(Long oid);
 	
 
 }
